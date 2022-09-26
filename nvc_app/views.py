202d8a122b70b42b983ae7b9f378fc108b7c86ce
aman <aman.pandey@tecblic.com>
2022-09-26 12:56:49 +0530

    return email on login

--- nvc_app/views.py
+++ nvc_app/views.py
@@ -54,7 +54,7 @@ class UserLoginView(APIView):
       response = {
         "email": serializer.data.get('email')
       }
-      return Response({'token':token,"user_email":response, 'msg':'Login Success'}, status=status.HTTP_200_OK)
+      return Response({'token':token,"user_details":response, 'msg':'Login Success'}, status=status.HTTP_200_OK)
     else:
       return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
 
