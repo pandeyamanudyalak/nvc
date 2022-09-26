10a0d4ef38bf11e8b2a3de1a4cdc6f579b36665a
aman <aman.pandey@tecblic.com>
2022-09-26 13:24:53 +0530

    return email on login

--- nvc/settings.py
+++ nvc/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://3054-59-144-124-86.eu.ngrok.io"
     "https://nvccrm.herokuapp.com/",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
@@ -87,12 +88,14 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://3054-59-144-124-86.eu.ngrok.io"
     "https://nvccrm.herokuapp.com/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
+    "https://3054-59-144-124-86.eu.ngrok.io",
     "https://nvccrm.herokuapp.com/",
 ]
 
