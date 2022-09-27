3367a147e05de1c491cce387dc38804980ddcdf2
aman <aman.pandey@tecblic.com>
2022-09-27 11:31:29 +0530

    new commit after model updation

--- nvc/settings.py
+++ nvc/settings.py
@@ -62,15 +62,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:8000",
  
-    "https://nvccrm.herokuapp.com/",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-]
+#     "https://nvccrm.herokuapp.com/",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
 
 
 
@@ -87,12 +87,12 @@ CORS_ALLOWED_ORIGINS = [
 # ]
 
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000"
    
-    "https://nvccrm.herokuapp.com/",
-]
+#     "https://nvccrm.herokuapp.com/",
+# ]
 
 
 ROOT_URLCONF = 'nvc.urls'
